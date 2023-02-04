838c102efcfe8bac0facb8ef5b25a29ad1cfee44
Joseph Cosentino <joecose@amazon.com>
2023-02-03 17:39:21 -0800

    chore: pmd unused import

--- src/integrationtests/java/com/aws/greengrass/integrationtests/SyncTest.java
+++ src/integrationtests/java/com/aws/greengrass/integrationtests/SyncTest.java
@@ -78,7 +78,6 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
