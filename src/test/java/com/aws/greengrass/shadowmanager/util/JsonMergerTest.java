22fe67257c5ec468a20653d8614138a8aed692d3
Joseph Cosentino <joecose@amazon.com>
2023-09-20 10:31:09 -0700

    chore: more test coverage for null

--- src/test/java/com/aws/greengrass/shadowmanager/util/JsonMergerTest.java
+++ src/test/java/com/aws/greengrass/shadowmanager/util/JsonMergerTest.java
@@ -47,7 +47,9 @@ class JsonMergerTest {
                 Arguments.of("GIVEN patch with null nodes, THEN removes all null nodes in source node", SOURCE_NODE_STRING, PATCH_NODE_WITH_NULL_FIELD_STRING, MERGED_NODE_WITHOUT_NULL_FIELD_STRING),
                 Arguments.of("GIVEN patch with new nodes in an array node, THEN replaces all the elements in the source with the elements in the patch", SOURCE_NODE_WITH_ARRAY_NODE_STRING, PATCH_NODE_WITH_ARRAY_VALUE_NODE_STRING, MERGED_NODE_WITH_UPDATED_ARRAY_NODE_STRING),
                 Arguments.of("GIVEN patch with empty state, THEN source node is cleared", SOURCE_NODE_STRING, EMPTY_DOCUMENT, EMPTY_DOCUMENT),
-                Arguments.of("GIVEN patch with null state, THEN source node is cleared", SOURCE_NODE_STRING, NULL_DOCUMENT, EMPTY_DOCUMENT)
+                Arguments.of("GIVEN patch with null state, THEN source node is cleared", SOURCE_NODE_STRING, NULL_DOCUMENT, EMPTY_DOCUMENT),
+                Arguments.of("GIVEN empty source and non-empty patch, THEN patch is the result", EMPTY_DOCUMENT, SOURCE_NODE_STRING, SOURCE_NODE_STRING),
+                Arguments.of("GIVEN null source and non-empty patch, THEN patch is the result", NULL_DOCUMENT, SOURCE_NODE_STRING, SOURCE_NODE_STRING)
         );
     }
 
