2d2945cb8652d9cbffc27c7d2723a9dd1053a233
Joseph Cosentino <joecose@amazon.com>
2023-09-20 10:17:09 -0700

    chore: empty state document test coverage

--- src/test/java/com/aws/greengrass/shadowmanager/util/JsonUtilTest.java
+++ src/test/java/com/aws/greengrass/shadowmanager/util/JsonUtilTest.java
@@ -13,11 +13,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
 
 import static com.aws.greengrass.shadowmanager.util.JsonUtil.getPayloadJson;
 import static com.aws.greengrass.shadowmanager.util.JsonUtil.validatePayloadSchema;
@@ -26,6 +29,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith({MockitoExtension.class, GGExtension.class})
@@ -127,5 +131,25 @@ class JsonUtilTest {
         assertThat(thrown.getErrorMessage(), is(notNullValue()));
         assertThat(thrown.getErrorMessage().getErrorCode(), is(400));
         assertThat(thrown.getErrorMessage().getMessage(), is("JSON contains too many levels of nesting; maximum is 6"));
+    }
+
+    @SuppressWarnings("PMD.UnusedPrivateMethod")
+    private static Stream<Arguments> emptyStateDocuments() {
+        return Stream.of(
+                Arguments.of("{\"state\": null}", true),
+                Arguments.of("{\"state\": {}}", true),
+                Arguments.of("{}", true),
+                Arguments.of("null", true),
+                Arguments.of("{\"state\": {\"field\":1}}", false),
+                Arguments.of("{\"field\":1}", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("emptyStateDocuments")
+    void GIVEN_empty_state_document_WHEN_check_is_empty_state_document_THEN_return_true(String json, boolean emptyDocumentExpected) throws IOException {
+        assertEquals(emptyDocumentExpected,
+                JsonUtil.isEmptyStateDocument(getPayloadJson(json.getBytes(StandardCharsets.UTF_8)).get()),
+                String.format("%s %sexpected to be empty", json, emptyDocumentExpected ? "" : "not "));
     }
 }
