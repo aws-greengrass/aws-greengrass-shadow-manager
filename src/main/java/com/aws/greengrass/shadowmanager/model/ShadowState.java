4a68d48aa2d0443236789dbe094f0267ab2d66ce
Joseph Cosentino <joecose@amazon.com>
2023-10-10 14:35:05 -0700

    chore: cleanup

--- src/main/java/com/aws/greengrass/shadowmanager/model/ShadowState.java
+++ src/main/java/com/aws/greengrass/shadowmanager/model/ShadowState.java
@@ -7,12 +7,10 @@ package com.aws.greengrass.shadowmanager.model;
 
 import com.aws.greengrass.shadowmanager.util.JsonMerger;
 import com.aws.greengrass.shadowmanager.util.JsonUtil;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.Iterator;
 
@@ -35,13 +33,6 @@ public class ShadowState {
     @JsonProperty(SHADOW_DOCUMENT_STATE_REPORTED)
     private JsonNode reported;
 
-    /**
-     * If true, this {@link ShadowState} represents the {"state": null} document, which resets shadow state.
-     */
-    @Setter
-    @JsonIgnore
-    private boolean clear;
-
     public ShadowState() {
         this(null, null);
     }
@@ -49,11 +40,6 @@ public class ShadowState {
     public ShadowState(final JsonNode desired, final JsonNode reported) {
         this.desired = nullIfEmpty(desired);
         this.reported = nullIfEmpty(reported);
-    }
-
-    private ShadowState(final JsonNode desired, final JsonNode reported, boolean clear) {
-        this(desired, reported);
-        this.clear = clear;
     }
 
     /**
@@ -64,8 +50,7 @@ public class ShadowState {
     public ShadowState deepCopy() {
         return new ShadowState(
                 isNullOrMissing(this.desired) ? this.desired : this.desired.deepCopy(),
-                isNullOrMissing(this.reported) ? this.reported : this.reported.deepCopy(),
-                this.clear);
+                isNullOrMissing(this.reported) ? this.reported : this.reported.deepCopy());
     }
 
     /**
@@ -118,10 +103,7 @@ public class ShadowState {
      * @return a JSON node containing the shadow state.
      */
     public JsonNode toJson() {
-        if (clear) {
-            return JsonUtil.OBJECT_MAPPER.nullNode();
-        }
-        final ObjectNode result = JsonUtil.OBJECT_MAPPER.createObjectNode();
+        final ObjectNode result = JsonUtil.createEmptyObject();
         if (this.desired != null) {
             result.set(SHADOW_DOCUMENT_STATE_DESIRED, this.desired);
         }
