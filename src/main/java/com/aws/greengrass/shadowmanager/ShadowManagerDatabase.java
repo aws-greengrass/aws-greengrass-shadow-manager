2d402a53a5f1d41fdc8ab7fd16ea99d96d488388
SimonManour <165569614+SimonManour@users.noreply.github.com>
2026-04-20 19:31:03 +0200

    Database validation on startup (#219)
    * run sample validation query during init
    * fix spotbugs warning REC_CATCH_EXCEPTION

--- src/main/java/com/aws/greengrass/shadowmanager/ShadowManagerDatabase.java
+++ src/main/java/com/aws/greengrass/shadowmanager/ShadowManagerDatabase.java
@@ -24,6 +24,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.stream.Stream;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -117,10 +119,19 @@ public class ShadowManagerDatabase implements Closeable {
         // Validate that after migration we're actually able to open and connect to the DB.
         // This may fail if closing the DB after migration failed for some reason.
         try {
-            try (Connection p = getPool().getConnection()) {}
+            try (Connection p = getPool().getConnection(); Statement st = p.createStatement()) {
+                st.execute("SELECT 1");
+                st.execute("CHECKPOINT");
+            }
             return true;
-        } catch (Exception e) {
+        } catch (SQLException e) {
             logger.atError().cause(e).log("Shadow manager DB could not be opened. Deleting and recreating it");
+            close();
+            return false;
+        } catch (Exception e) {
+            logger.atError().cause(e).log(
+                "Shadow manager DB could not be opened (generic exception): " + e.getMessage()
+            );
             close();
             return false;
         }
